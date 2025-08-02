9d2513be2cedd6b95a7f6a88ea1ff053eb26758f
A-Buren <burenkov2285@gmail.com>
2025-08-02 13:52:38 +0300

    Добавил оформление через allure

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,5 +1,6 @@
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
+import allure
 
 
 class LoginPageLocators:
@@ -36,11 +37,17 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.YANDEX_LOGIN_BUTTON)
 
 
+    @allure.step('Нажимаем на кнопку "Войти в Одноклассники"')
     def click_login(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
 
+    @allure.step('Вводим логин в поле "Телефон или адрес эл. почты"')
     def input_login_without_password(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys("абырвалг@mail.ru")
 
+    @allure.step('Получаем текст ошибки')
     def get_error_text(self):
+        self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
